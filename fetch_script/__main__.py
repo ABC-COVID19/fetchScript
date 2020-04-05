a844b14489ba91fd1ab58b4865f824c84271c836
Tiago Rosa <secknv@gmail.com>
2020-04-05 19:53:44 +0100

    test version that includes categories and Atypes generator

--- fetch_script/__main__.py
+++ fetch_script/__main__.py
@@ -22,7 +22,7 @@ def fetch_articles_pubmed(icam, num_articles, search_term):
     print(new, 'new articles!')
     print('starting push!' if new else 'no new articles!')
     for entry in new_articles_ids:
-
+        print(f'submitting pubmed id #{entry}')
         r = icam.post_new_articles(pubmed.get_single_article(entry), icam.get_srepo_id('pubmed'))
         # If we didn't get a 201 back, log the problem to console
         if r.status_code != 201:
@@ -43,6 +43,7 @@ def main():
 
     icam = Icam(gateway, user, password)
 
+    # icam.ctrees_testhook()
     fetch_articles_pubmed(icam, num_articles, search_term)
 
 
